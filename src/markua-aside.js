abc14c9885913c75963e64b54a28abee5160af6f
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2022-04-04 09:45:49 -0700

    feat: Support header style (A>, B>) asides and blurbs

--- src/markua-aside.js
+++ src/markua-aside.js
@@ -13,14 +13,6 @@
 //-----------------------------------------------------------------------------
 
 const TAGS = new Set(["aside", "blurb"]);
-const BLURB_CLASSES = new Set([
-    "center",
-    "discussion",
-    "error",
-    "information",
-    "tip",
-    "warning"
-]);
 
 const BLOCKS = new Map([
     ["A>", "aside"],
@@ -85,7 +77,7 @@ function getLine(state, lineNumber) {
 
 }
 
-function findBlock(state, lineNumber) {
+function findBlockHeader(state, lineNumber) {
 
     const {
         text,
@@ -100,15 +92,17 @@ function findBlock(state, lineNumber) {
     }
 
     const [ tagName, className ] = blockType.split(":");
-    
+    const c = text[2];
+    const hasTrailingSpace = isSpace(c);
+
     return {
         tagName,
         className,
         marker,
         start: lineStart,
-        end: lineStart + 2,
+        end: lineStart + 2 + (hasTrailingSpace ? 1 : 0),
         lineNumber,
-        spaceAfter: isSpace(text[2])
+        spaceAfter: hasTrailingSpace
     };
 }
 
@@ -163,9 +157,9 @@ function findStartTag(state, lineNumber) {
                 className += src.charAt(i);
             }
 
-            if (!BLURB_CLASSES.has(className)) {
-                console.warn("Unknown blurb class detected:", className);
-            }
+            // if (!BLURB_CLASSES.has(className)) {
+            //     console.warn("Unknown blurb class detected:", className);
+            // }
 
             expectedClosingCurlyPos = i - lineStart;
         }
@@ -222,280 +216,61 @@ function findEndTag(state, lineNumber, tagName) {
  *      false if not. 
  */
 
-function asideBlockPlugin(state, block, endLine, silent) {
+function asideBlockHeaderPlugin(state, header, endLine, silent) {
 
-    let startLine = block.lineNumber;
-
-    var adjustTab,
-        ch,
-        i,
-        initial,
-        l,
-        lastLineEmpty,
-        lines,
-        nextLine,
-        offset,
-        oldBMarks,
-        oldBSCount,
-        oldIndent,
-        oldParentType,
-        oldSCount,
-        oldTShift,
-        spaceAfterMarker,
-        terminate,
-        terminatorRules,
-        token,
-        isOutdented,
-        oldLineMax = state.lineMax,
-        pos = state.bMarks[startLine] + state.tShift[startLine],
-        max = state.eMarks[startLine];
+    const oldLineMax = state.lineMax;
+    let startLine = header.lineNumber;
+    // let pos = state.bMarks[startLine] + state.tShift[startLine];
+    let pos = header.end;
 
     if (silent) {
         return true;
     }
 
-    // set offset past spaces and "A>"
-    initial = offset = state.sCount[startLine] + 2;
-
-    // skip one optional space after '>'
-    if (state.src.charCodeAt(pos) === 0x20 /* space */) {
-        // ' >   test '
-        //     ^ -- position start of line here:
-        pos++;
-        initial++;
-        offset++;
-        adjustTab = false;
-        spaceAfterMarker = true;
-    } else if (state.src.charCodeAt(pos) === 0x09 /* tab */) {
-        spaceAfterMarker = true;
-
-        if ((state.bsCount[startLine] + offset) % 4 === 3) {
-            // '  >\t  test '
-            //       ^ -- position start of line here (tab has width===1)
-            pos++;
-            initial++;
-            offset++;
-            adjustTab = false;
-        } else {
-            // ' >\t  test '
-            //    ^ -- position start of line here + shift bsCount slightly
-            //         to make extra space appear
-            adjustTab = true;
-        }
-    } else {
-        spaceAfterMarker = false;
-    }
-
-    oldBMarks = [state.bMarks[startLine]];
+    // adjust the start of the line to be after the A>
+    let oldBMarks = [state.bMarks[startLine]];
     state.bMarks[startLine] = pos;
 
-    while (pos < max) {
-        ch = state.src.charCodeAt(pos);
+    // save new parent type
+    const oldParentType = state.parentType;
+    state.parentType = header.tagName;
 
-        if (isSpace(ch)) {
-            if (ch === 0x09) {
-                offset += 4 - (offset + state.bsCount[startLine] + (adjustTab ? 1 : 0)) % 4;
-            } else {
-                offset++;
-            }
-        } else {
-            break;
-        }
-
-        pos++;
-    }
-
-    oldBSCount = [state.bsCount[startLine]];
-    state.bsCount[startLine] = state.sCount[startLine] + 1 + (spaceAfterMarker ? 1 : 0);
-
-    lastLineEmpty = pos >= max;
-
-    oldSCount = [state.sCount[startLine]];
-    state.sCount[startLine] = offset - initial;
-
-    oldTShift = [state.tShift[startLine]];
-    state.tShift[startLine] = pos - state.bMarks[startLine];
-
-    // same terminator rules as block quotes
-    terminatorRules = state.md.block.ruler.getRules('blockquote');
-
-    oldParentType = state.parentType;
-    state.parentType = block.tagName;
-
-    // Search the end of the block
-    //
-    // Block ends with either:
-    //  1. an empty line outside:
-    //     ```
-    //     > test
-    //
-    //     ```
-    //  2. an empty line inside:
-    //     ```
-    //     >
-    //     test
-    //     ```
-    //  3. another tag:
-    //     ```
-    //     > test
-    //      - - -
-    //     ```
+    // search for the end of the block
+    let nextLine;
     for (nextLine = startLine + 1; nextLine < endLine; nextLine++) {
-        // check if it's outdented, i.e. it's inside list item and indented
-        // less than said list item:
-        //
-        // ```
-        // 1. anything
-        //    > current blockquote
-        // 2. checking this line
-        // ```
-        isOutdented = state.sCount[nextLine] < state.blkIndent;
 
-        pos = state.bMarks[nextLine] + state.tShift[nextLine];
-        max = state.eMarks[nextLine];
+        const nextHeader = findBlockHeader(state, nextLine);
 
-        if (pos >= max) {
-            // Case 1: line is not inside the blockquote, and this line is empty.
+        // no aside/blurb or different aside/blurb
+        if (!nextHeader || nextHeader.tagName !== header.tagName || nextHeader.className !== header.className) {
             break;
         }
 
-        if (state.src.charCodeAt(pos++) === 0x3E/* > */ && !isOutdented) {
-            // This line is inside the blockquote.
-
-            // set offset past spaces and ">"
-            initial = offset = state.sCount[nextLine] + 1;
-
-            // skip one optional space after '>'
-            if (state.src.charCodeAt(pos) === 0x20 /* space */) {
-                // ' >   test '
-                //     ^ -- position start of line here:
-                pos++;
-                initial++;
-                offset++;
-                adjustTab = false;
-                spaceAfterMarker = true;
-            } else if (state.src.charCodeAt(pos) === 0x09 /* tab */) {
-                spaceAfterMarker = true;
-
-                if ((state.bsCount[nextLine] + offset) % 4 === 3) {
-                    // '  >\t  test '
-                    //       ^ -- position start of line here (tab has width===1)
-                    pos++;
-                    initial++;
-                    offset++;
-                    adjustTab = false;
-                } else {
-                    // ' >\t  test '
-                    //    ^ -- position start of line here + shift bsCount slightly
-                    //         to make extra space appear
-                    adjustTab = true;
-                }
-            } else {
-                spaceAfterMarker = false;
-            }
-
-            oldBMarks.push(state.bMarks[nextLine]);
-            state.bMarks[nextLine] = pos;
-
-            while (pos < max) {
-                ch = state.src.charCodeAt(pos);
-
-                if (isSpace(ch)) {
-                    if (ch === 0x09) {
-                        offset += 4 - (offset + state.bsCount[nextLine] + (adjustTab ? 1 : 0)) % 4;
-                    } else {
-                        offset++;
-                    }
-                } else {
-                    break;
-                }
-
-                pos++;
-            }
-
-            lastLineEmpty = pos >= max;
-
-            oldBSCount.push(state.bsCount[nextLine]);
-            state.bsCount[nextLine] = state.sCount[nextLine] + 1 + (spaceAfterMarker ? 1 : 0);
-
-            oldSCount.push(state.sCount[nextLine]);
-            state.sCount[nextLine] = offset - initial;
-
-            oldTShift.push(state.tShift[nextLine]);
-            state.tShift[nextLine] = pos - state.bMarks[nextLine];
-            continue;
-        }
-
-        // Case 2: line is not inside the blockquote, and the last line was empty.
-        if (lastLineEmpty) { break; }
-
-        // Case 3: another tag found.
-        terminate = false;
-        for (i = 0, l = terminatorRules.length; i < l; i++) {
-            if (terminatorRules[i](state, nextLine, endLine, true)) {
-                terminate = true;
-                break;
-            }
-        }
-
-        if (terminate) {
-            // Quirk to enforce "hard termination mode" for paragraphs;
-            // normally if you call `tokenize(state, startLine, nextLine)`,
-            // paragraphs will look below nextLine for paragraph continuation,
-            // but if blockquote is terminated by another tag, they shouldn't
-            state.lineMax = nextLine;
-
-            if (state.blkIndent !== 0) {
-                // state.blkIndent was non-zero, we now set it to zero,
-                // so we need to re-calculate all offsets to appear as
-                // if indent wasn't changed
-                oldBMarks.push(state.bMarks[nextLine]);
-                oldBSCount.push(state.bsCount[nextLine]);
-                oldTShift.push(state.tShift[nextLine]);
-                oldSCount.push(state.sCount[nextLine]);
-                state.sCount[nextLine] -= state.blkIndent;
-            }
-
-            break;
-        }
-
+        // otherwise update line markers
         oldBMarks.push(state.bMarks[nextLine]);
-        oldBSCount.push(state.bsCount[nextLine]);
-        oldTShift.push(state.tShift[nextLine]);
-        oldSCount.push(state.sCount[nextLine]);
-
-        // A negative indentation means that this is a paragraph continuation
-        //
-        state.sCount[nextLine] = -1;
+        state.bMarks[nextLine] = nextHeader.end;
     }
 
-    oldIndent = state.blkIndent;
+    // save old indent
+    const oldIndent = state.blkIndent;
     state.blkIndent = 0;
-    const tagName = block.tagName;
 
-    token = state.push(`${tagName}_open`, tagName, 1);
-    token.markup = block.marker;
-    token.map = lines = [startLine, 0];
-    token.info = tagName === "blurb" ? { className: block.className } : null;
+    const tagName = header.tagName;
+    let token = state.push(`${tagName}_open`, tagName, 1);
+    token.markup = header.marker;
+    // token.map = lines = [startLine, 0];
+    token.info = tagName === "blurb" ? { className: header.className } : null;
 
     state.md.block.tokenize(state, startLine, nextLine);
 
     token = state.push(`${tagName}_close`, tagName, -1);
-    token.markup = block.marker;
+    token.markup = header.marker;
 
     state.lineMax = oldLineMax;
     state.parentType = oldParentType;
-    lines[1] = state.line;
-
-    // Restore original tShift; this might not be necessary since the parser
-    // has already been here, but just to make sure we can do that.
-    for (i = 0; i < oldTShift.length; i++) {
-        state.bMarks[i + startLine] = oldBMarks[i];
-        state.tShift[i + startLine] = oldTShift[i];
-        state.sCount[i + startLine] = oldSCount[i];
-        state.bsCount[i + startLine] = oldBSCount[i];
-    }
     state.blkIndent = oldIndent;
+
+    return true;
 }
 
 /**
@@ -617,9 +392,9 @@ export function asidePlugin(md) {
             return false;
         }
 
-        const block = findBlock(state, startLine);
+        const block = findBlockHeader(state, startLine);
         if (block) {
-            return asideBlockPlugin(state, block, endLine, silent);
+            return asideBlockHeaderPlugin(state, block, endLine, silent);
         }
 
         const startTag = findStartTag(state, startLine);
